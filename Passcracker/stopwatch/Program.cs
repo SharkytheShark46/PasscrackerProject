e8fd2910438c04854d64fa0f92dc3ad4fd7135e8
SharkytheShark46 <introsuz46az@gmail.com>
2025-03-27 13:28:46 +0300

    First project

---
+++ Passcracker/stopwatch/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            for(int i = 0; i < 9999999; ++i)
+            { }
+            stopWatch.Stop();
+
+            TimeSpan ts = stopWatch.Elapsed;
+
+            string elapsedTime = String.Format("{0:00}:{1:00}",
+                ts.Seconds,
+                ts.Milliseconds);
+            Console.WriteLine("RunTime " + elapsedTime);
+        }
+    }
+}
